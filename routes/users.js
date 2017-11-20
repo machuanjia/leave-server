1869d72da31c67db8b74af200711332c44f68a75
machuanjia <yanshi0429@126.com>
2017-11-20 08:34:59 +0800

    add

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.post('/login',function(req, res, next) {
+    return res.send({data:{
+        password:123456
+    },code:200});
+});
+
+module.exports = router;
