156005c35c288bc7b631dbead11eda11688e848f
machuanjia <machuanjia@worktile.com>
2017-12-05 10:26:48 +0800

    add

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
-router.post('/login',function(req, res, next) {
-    return res.send({data:{
-        password:123456
-    },code:200});
+router.post('/login', function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  return res.send({data: {password: 123456}, code: 200});
 });
+
 
 module.exports = router;
